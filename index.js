545aed349c2dfe43eeb559ceab12e89fa51e7fc3
GeorgeSato <74667877+GeorgeSato@users.noreply.github.com>
2020-12-04 18:15:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const client = redis.createClient({
    port :6379,
    host : '127.0.0.1',
-   password : 'jorge'
+   password : process.env.REDIS_PASSWORD
 }
 
 
